b30d197676c88039ed5a1d613fdc50281cc3bac6
purplegrape <purplegrape4@gmail.com>
2024-12-11 10:34:32 +0800

    update

--- docker/3.1/entrypoint.sh
+++ docker/3.1/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 if [ ! -d /data/itop ];then
-	unzip -qn -d /data /opt/iTop-3.1.2-14913.zip "web/*"
+	unzip -qn -d /data /opt/iTop-3.1.zip "web/*"
 	mv /data/web /data/itop
 	mkdir -p /data/itop/{conf,data,log,env-production,env-production-build,env-test,env-test-build}
 	chown -R www-data.www-data /data/itop/{conf,data,log,env-production,env-production-build,env-test,env-test-build}
